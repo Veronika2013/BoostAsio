ef7b5d2116ed313fa04b79e7d44e03b99b7c1088
Veronika2013 <veronikauskova550@gmail.com>
2021-01-15 22:40:35 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <fstream>
+#include <istream>
+#include <ostream>
+#include <string>
+#include <boost/asio.hpp>
+using namespace boost::asio;
+
+class ShipmentInServer {
+public:
+    void shipment_main_void()
+    {
+        io_service service;
+        ip::tcp::resolver resolver(service);
+        ip::tcp::resolver::query query("www.b24-8dd5ja.bitrix24.site", "80");
+        ip::tcp::resolver::iterator iter = resolver.resolve(query);
+        ip::tcp::endpoint ep = *iter; 
+        std::cout << ep.address().to_string() << std::endl;
+        
+
+    }
+    void protected_channel()
+    {
+
+    }
+};
+
+class GetDataInServver {
+   
+
+};
+int main()
+{
+    ShipmentInServer obj;
+    obj.shipment_main_void();
+    return 0;
+}
